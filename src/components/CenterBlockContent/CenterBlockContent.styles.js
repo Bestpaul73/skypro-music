21141598e89ef6cf0d8aeb38fbdc4d1080a0894a
Paul Palkin <bestpaul@yandex.ru>
2023-10-22 18:40:49 +0300

    HW4-2

--- src/components/CenterBlockContent/CenterBlockContent.styles.js
+++ src/components/CenterBlockContent/CenterBlockContent.styles.js
@@ -181,7 +181,7 @@ export const Rectangle12Div = styled.div`
   background: #313131;
 `
 export const Rectangle13Div = styled.div`
-  width: 225px;
+  width: 224px;
   height: 19px;
   flex-shrink: 0;
   background: #313131;
